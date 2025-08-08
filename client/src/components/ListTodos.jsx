00aab288f355d680b927e61bd427d255c42bc095
Shin <fgcaraan@gbox.com>
2025-08-08 23:42:18 +0800

    Added a frontend

---
+++ client/src/components/ListTodos.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+
+function ListTodos() {
+
+    const [todos, setTodos] = useState([]);
+
+    // delete function
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE",
+            })
+
+            console.log(deleteTodo);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    const getTodos = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/todos");
+            const jsonData = await response.json();
+            
+            setTodos(jsonData);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    // console.log(todos);
+
+    return(
+        <>
+            <h2 className="mt-5 text-center">List Todo</h2>
+            <table className="table mt-5 text-center">
+                <thead>
+                    <tr>
+                        <th>Description</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {todos.map(todo => (
+                        <tr key={todo.todo_id}>
+                            <td>{todo.description}</td>
+                            <td><button>Edit</button></td>
+                            <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
+}
+
+export default ListTodos
